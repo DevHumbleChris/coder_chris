133abc3967b9910e5549554911ad7a047cae1e4a
DevHumbleChris <dev.humblechris254@gmail.com>
2022-01-27 15:22:32 +0300

    updated the index.html file

--- coder_chris/settings.py
+++ coder_chris/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-5j!&w160sq&f6_^v&rr2@=*u7y%w#2^w5y9h*!)h%8s^nt%ud!'
+SECRET_KEY = 'django-insecure-5j!&w160sq&f6_^v&rr2@=*u7y%w#2^w5y9h*!)h%8s^nt%ud!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
